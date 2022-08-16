83e4c669f6627964c6e3c31ff037c6370dd69379
Wangzhijie1209 <2201792896@qq.com>
2022-08-16 16:08:30 +0800

    RecyclerView的基本使用

---
+++ app/src/main/java/com/example/myapplication/recyclerview/FruitAdapter.java
@@ -0,0 +1,81 @@
+package com.example.myapplication.recyclerview;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myapplication.R;
+
+import java.util.List;
+
+public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.ViewHolder> {
+    private List<Fruit> mFruitList;
+
+    /**
+     * 构造函数,用于把要展示的数据源传进来,并赋值给一个全局变量mFruitList,后续的操作都将在这个数据源的基础上进行
+     *
+     * @param mFruitList
+     */
+    public FruitAdapter(List<Fruit> mFruitList) {
+        this.mFruitList = mFruitList;
+    }
+
+
+    /**
+     * onCreateViewHolder()方法是用于创建ViewHolder实例的,我们在这个方法中将fruit_item布局加载进来,然后创建一个
+     * ViewHolder实例,并把加载出来的布局传入到构造函数当中,最后将viewHolder的实例返回
+     * @param parent
+     * @param viewType
+     * @return
+     */
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fruit_item, parent, false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
+    }
+
+    /**
+     * 绑定数据
+     * @param holder
+     * @param position
+     */
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Fruit fruit = mFruitList.get(position);
+        holder.image_view.setImageResource(fruit.getImageId());
+        holder.text_view.setText(fruit.getName());
+    }
+
+    /**
+     * 用于告诉RecyclerView一共有多少子项,直接返回数据源的长度即可
+     * @return
+     */
+    @Override
+    public int getItemCount() {
+        return mFruitList.size();
+    }
+
+    /**
+     * 定义了一个内部类ViewHolder,继承至RecyclerView.ViewHolder,ViewHolder的构造函数中要传入一个View参数,
+     * 这个参数通常就是RecyclerView子项的最外层布局,也就是Item布局
+     * 通过findViewById()方法来获取布局中的空间
+     */
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private final ImageView image_view;
+        private final TextView text_view;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            text_view = itemView.findViewById(R.id.text_view);
+            image_view = itemView.findViewById(R.id.image_view);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/recyclerview/RecyclerViewActivity.java
@@ -0,0 +1,53 @@
+package com.example.myapplication.recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.myapplication.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecyclerViewActivity extends AppCompatActivity {
+
+    private List<Fruit> fruitList = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler_view);
+        initFruits();//初始化水果数据
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        FruitAdapter fruitAdapter = new FruitAdapter(fruitList);
+        recyclerView.setAdapter(fruitAdapter);
+    }
+
+    private void initFruits() {
+        for (int i = 0; i < 2; i++) {
+            Fruit kiwifruit = new Fruit("kiwifruit", R.drawable.kiwifruit);
+            fruitList.add(kiwifruit);
+            Fruit litchi = new Fruit("litchi", R.drawable.litchi);
+            fruitList.add(litchi);
+            Fruit lemon = new Fruit("lemon", R.drawable.lemon);
+            fruitList.add(lemon);
+            Fruit mango = new Fruit("mango", R.drawable.mango);
+            fruitList.add(mango);
+            Fruit mangosteen = new Fruit("mangosteen", R.drawable.mangosteen);
+            fruitList.add(mangosteen);
+            Fruit persimmon = new Fruit("persimmon", R.drawable.persimmon);
+            fruitList.add(persimmon);
+            Fruit strawberry = new Fruit("strawberry", R.drawable.strawberry);
+            fruitList.add(strawberry);
+            Fruit watermelon = new Fruit("watermelon", R.drawable.watermelon);
+            fruitList.add(watermelon);
+            Fruit waxapple = new Fruit("waxapple", R.drawable.waxapple);
+            fruitList.add(waxapple);
+        }
+    }
+}
