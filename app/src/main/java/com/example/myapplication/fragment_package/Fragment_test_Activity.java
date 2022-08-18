5fd82b55260c94b6a342b133994242e0f0211c43
Wangzhijie1209 <2201792896@qq.com>
2022-08-19 01:11:23 +0800

    动态添加碎片

--- app/src/main/java/com/example/myapplication/fragment_package/Fragment_test_Activity.java
+++ app/src/main/java/com/example/myapplication/fragment_package/Fragment_test_Activity.java
@@ -1,16 +1,58 @@
 package com.example.myapplication.fragment_package;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.myapplication.R;
 
-public class Fragment_test_Activity extends AppCompatActivity {
+/**
+ *在左侧碎片中的按钮注册了一个点击事件,然后调用replaceFragment()方法添加了RightFragment这个碎片,当点击左侧碎片中
+ * 的按钮时,又会调用replaceFragment()方法将右侧碎片替换成AnotherRightFragment。
+ */
+public class Fragment_test_Activity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment_test);
+        Button button = findViewById(R.id.left_fragment_button);
+        button.setOnClickListener(this);
+        replaceFragment(new RightFragment());
+
+    }
+
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.left_fragment_button:
+                replaceFragment(new AnotherRightFragment());
+                break;
+            default:
+                break;
+        }
+    }
+
+    /**
+     * 动态添加碎片分为5步:
+     * 1.创建待添加的碎片实例
+     * 2.获取FragmentManager,在活动中可以直接通过调用getSupportFragmentManager()方法得到
+     * 3.开启一个事务,通过调用beginTransaction()方法开启
+     * 4.向容器内添加或替换碎片,一般使用replace()方法实现,需要传入容器的id和待添加的碎片实例
+     * 5.提交事务,调用commit()方法来完成
+     * @param fragment
+     */
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.right_fragment,fragment);
+        transaction.commit();
     }
 }
