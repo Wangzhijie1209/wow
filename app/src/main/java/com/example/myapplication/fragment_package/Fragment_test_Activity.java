468461ac23ce1ff3d7cef7eabbb27995d3ad264a
Wangzhijie1209 <2201792896@qq.com>
2022-08-19 00:47:17 +0800

    Fragment的基本使用

---
+++ app/src/main/java/com/example/myapplication/fragment_package/Fragment_test_Activity.java
@@ -0,0 +1,16 @@
+package com.example.myapplication.fragment_package;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myapplication.R;
+
+public class Fragment_test_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment_test);
+    }
+}
